a4556badd9568084221481808dce8408642b8dea
KurisuLim <kazuki1002003@yahoo.com>
2020-02-08 12:07:30 -0800

    adds printArrayInt method

--- src/Homework1/SimpleJavaExercises.java
+++ src/Homework1/SimpleJavaExercises.java
@@ -10,15 +10,26 @@ public class SimpleJavaExercises {
         *   3. Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
         *   4. Call index outside of the bound. Handle this exception printing “Outside of bound message.”
         * */
-
+        int[] num = {1,2,3,4,5};
         skipNumber(7, 10); // # 2
+        printArrayInt(num);
     }
 
     public static void skipNumber(int min, int max){
         for(int i = 1;  i <= max; i++){
             if(i != min){
                 System.out.println("Counting: " + i);
+                // or we can just use the keyword "Contiue"
+                //if (i == 7){ continue;}
             }
         }
     }
+
+    public static void printArrayInt(int[] arrNum){
+        for(int i = 0; i < arrNum.length; i++){
+            System.out.println(arrNum[i]);
+        }
+    }
+
+
 }
