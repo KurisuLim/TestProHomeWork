3107385c06cd7664d388b4000ff12a920255bf44
KurisuLim <kazuki1002003@yahoo.com>
2020-02-08 14:23:34 -0800

    adds fibonacci exercises

--- src/Homework2/Loops.java
+++ src/Homework2/Loops.java
@@ -5,5 +5,22 @@ public class Loops {
         /*
         * Waiting for the question to show up
         * */
+        fibonacci(20);
+    }
+    /*
+    * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, …
+    * */
+
+    public static void fibonacci(int num){
+        int first =0;
+        int second = 1;
+        int next = 1;
+
+        for(int i = 2; i <= num; i++){
+            next = second + first;
+            first = second;
+            second = next;
+            System.out.print(next + ",");
+        }
     }
 }
