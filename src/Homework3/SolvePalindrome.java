c66aac130e91b5c0c67ce9f7964c8e252e95e981
KurisuLim <kazuki1002003@yahoo.com>
2020-02-12 17:46:38 -0800

    adds regex for capital letters

--- src/Homework3/SolvePalindrome.java
+++ src/Homework3/SolvePalindrome.java
@@ -6,10 +6,11 @@ public class SolvePalindrome {
     public static void main(String[] args){
         isPalindrome("race cars"); //not palindrome
         isPalindrome("race car ! @#$%^&*"); //palindrome
+        isPalindrome("Race caR"); //palindrome
     }
     public static void isPalindrome(String word){
         String reverseWord = "";
-        String sanitizeWord = word.replaceAll("[!.,?@#$%^&*-+^\\s]","");
+        String sanitizeWord = word.replaceAll("[A-Z!.,?@#$%^&*-+^\\s]","");
         System.out.println("\nCurrent Word: " + sanitizeWord + "\n");
         for(int i = sanitizeWord.length() - 1; i >= 0; i--){
             reverseWord += sanitizeWord.charAt(i);
