7074125311750b6ca195099e44a77fa09ddb4290
KurisuLim <kazuki1002003@yahoo.com>
2020-02-15 14:54:42 -0800

    removes regex for capital letters and add method for lower case

--- src/WeekTwo/Homework3/SolvePalindrome.java
+++ src/WeekTwo/Homework3/SolvePalindrome.java
@@ -2,13 +2,14 @@ package WeekTwo.Homework3;
 
 public class SolvePalindrome {
     public static void main(String[] args){
-        isPalindrome("race cars"); //not palindrome
-        isPalindrome("race car ! @#$%^&*"); //palindrome
-        isPalindrome("Race caR"); //palindrome
+//        isPalindrome("race cars"); //not palindrome
+//        isPalindrome("race car ! @#$%^&*"); //palindrome
+//        isPalindrome("Race caR"); //palindrome
+        isPalindrome("Was it a cat I saw");
     }
     public static void isPalindrome(String word){
         String reverseWord = "";
-        String sanitizeWord = word.replaceAll("[A-Z!.,?@#$%^&*-+^\\s]","");
+        String sanitizeWord = word.toLowerCase().replaceAll("[!.,?@#$%^&*-+^\\s]","");
         System.out.println("\nCurrent Word: " + sanitizeWord + "\n");
         for(int i = sanitizeWord.length() - 1; i >= 0; i--){
             reverseWord += sanitizeWord.charAt(i);
