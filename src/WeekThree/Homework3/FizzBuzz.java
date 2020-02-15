a8f79b3c3ed2f762c18fb662b7f456931a11054f
KurisuLim <kazuki1002003@yahoo.com>
2020-02-15 14:09:56 -0800

    add fizzbuzz for weekr 3: homework 3

--- src/WeekThree/Homework3/FizzBuzz.java
+++ src/WeekThree/Homework3/FizzBuzz.java
@@ -4,9 +4,30 @@ public class FizzBuzz {
     /*
         Week 3 – Homework 3 – Interview Code exercise – FizzBuzz
 
-        Write a program that prints the numbers from 1 to 100.
-        But for multiples of three print “Fizz” instead of the number
-        and for the multiples of five print “Buzz”. For numbers which
-        are multiples of both three and five print “FizzBuzz”.
+       1. Write a program that prints the numbers from 1 to 100.
+       2. But for multiples of three print “Fizz” instead of the number
+       3. and for the multiples of five print “Buzz”. For numbers which
+       4. are multiples of both three and five print “FizzBuzz”.
     */
+
+    public static void main(String[] args){
+        fizzBuzz(100);
+    }
+
+    public static void fizzBuzz(int num){
+
+        for(int i = 1; i <= num; i++){
+//            System.out.println(i);
+          if(i % 3 == 0 && i %5 == 0){
+                System.out.println("Number: " + i + " is Fizz Buzz!");
+            }
+            else if(i %3 == 0){
+                System.out.println("Number: " + i + " is Fizz!");
+            }
+            else if(i %5 == 0){
+                System.out.println("Number: " + i + " is Buzz!");
+            }
+
+        }
+    }
 }
