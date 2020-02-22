6b9577afeb87906b68c17356f0e783965027581e
KurisuLim <kazuki1002003@yahoo.com>
2020-02-22 14:16:43 -0800

    refactor fizzbuzz methid

--- src/WeekFour/HomeworkRecursion.java
+++ src/WeekFour/HomeworkRecursion.java
@@ -1,14 +1,12 @@
 package WeekFour;
 
-import java.io.PrintStream;
-
 public class HomeworkRecursion {
     /*
     *   Algorithms Homework – Recursion
     *   Recursion
     *   Solve FizzBuzz and Palindrome exercise using recursion.
     * */
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         //Recursion Palindrome
         System.out.println("Recursion Palindrome:");
@@ -18,7 +16,7 @@ public class HomeworkRecursion {
 
         //Recursion Fizzbuzz
         System.out.println("\nRecursion Fizzbuzz:");
-        fizzBuzz(1,15);
+        fizzBuzz(100);
     }
 
     public static boolean isPalindrome(String word){
@@ -32,36 +30,36 @@ public class HomeworkRecursion {
         }
 
         String newWord = word.toLowerCase().replaceAll("[!.,?@#$%^&*-+^\\s]","");
-        int newWordLength = newWord.length();
-        String firstChar = newWord.substring(0,1);
-        String endChar = newWord.substring(newWord.length()-1, newWordLength);
 
-        if(!firstChar.equals(endChar)){
-            return false;
-        }else{
+        if(newWord.charAt(0) == newWord.charAt(newWord.length() -1 )){
             return isPalindrome(newWord.substring(1, newWord.length()-1));
         }
 
-    }
-
-    public static void fizzBuzz(int startNum, int endNum){
-
-        if(startNum % 15 == 0){
-            System.out.println("Fizz Buzz");
-        } else if(startNum % 3 == 0){
-            System.out.println("Fizz");
-        } else if(startNum % 5 == 0){
-            System.out.println("Buzz");
-        }else{
-            System.out.println(startNum);
-        }
-        startNum++;
-
-        if(startNum <= endNum) {
-            fizzBuzz(startNum,endNum);
-        }
+        return false;
 
     }
+
+    public static void fizzBuzz(int endNum) {
+
+        if (endNum != 0) {
+
+            if (endNum % 15 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (endNum % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (endNum % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(endNum);
+            }
+
+            endNum--;
+            fizzBuzz(endNum);
+        }
+
+
+    }
+
 }
 
 
